6d5bf6cfbff842cf6c6bda5b7add98d36b851099
unknown <dzulfiqaralang@gmail.com>
2024-11-04 20:12:40 +0700

    first commit

---
+++ application/views/customer/dashboard.php
@@ -0,0 +1,194 @@
+<!-- Page Content -->
+<!-- Banner Starts Here -->
+<div class="main-banner header-text">
+  <div class="container-fluid">
+    <div class="owl-banner owl-carousel">
+      <div class="item">
+        <img src="<?= base_url('assets/assets_shop') ?>/images/product-1-720x480.jpg" alt="">
+        <div class="item-content">
+          <div class="main-content">
+            <div class="meta-category">
+              <span>
+                <i class="fa fa-user"></i> 5 &nbsp;&nbsp;&nbsp;
+                <i class="fa fa-briefcase"></i> 4 &nbsp;&nbsp;&nbsp;
+                <i class="fa fa-sign-out"></i> 4 &nbsp;&nbsp;&nbsp;
+                <i class="fa fa-cog"></i> A
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="item">
+        <img src="<?= base_url('assets/assets_shop') ?>/images/product-2-720x480.jpg" alt="">
+        <div class="item-content">
+          <div class="main-content">
+            <div class="meta-category">
+              <span>
+                <i class="fa fa-user"></i> 5 &nbsp;&nbsp;&nbsp;
+                <i class="fa fa-briefcase"></i> 4 &nbsp;&nbsp;&nbsp;
+                <i class="fa fa-sign-out"></i> 4 &nbsp;&nbsp;&nbsp;
+                <i class="fa fa-cog"></i> A
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="item">
+        <img src="<?= base_url('assets/assets_shop') ?>/images/product-3-720x480.jpg" alt="">
+        <div class="item-content">
+          <div class="main-content">
+            <div class="meta-category">
+              <span>
+                <i class="fa fa-user"></i> 5 &nbsp;&nbsp;&nbsp;
+                <i class="fa fa-briefcase"></i> 4 &nbsp;&nbsp;&nbsp;
+                <i class="fa fa-sign-out"></i> 4 &nbsp;&nbsp;&nbsp;
+                <i class="fa fa-cog"></i> A
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="item">
+        <img src="<?= base_url('assets/assets_shop') ?>/images/product-4-720x480.jpg" alt="">
+        <div class="item-content">
+          <div class="main-content">
+            <div class="meta-category">
+              <span>
+                <i class="fa fa-user"></i> 5 &nbsp;&nbsp;&nbsp;
+                <i class="fa fa-briefcase"></i> 4 &nbsp;&nbsp;&nbsp;
+                <i class="fa fa-sign-out"></i> 4 &nbsp;&nbsp;&nbsp;
+                <i class="fa fa-cog"></i> A
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="item">
+        <img src="<?= base_url('assets/assets_shop') ?>/images/product-5-720x480.jpg" alt="">
+        <div class="item-content">
+          <div class="main-content">
+            <div class="meta-category">
+              <span>
+                <i class="fa fa-user"></i> 5 &nbsp;&nbsp;&nbsp;
+                <i class="fa fa-briefcase"></i> 4 &nbsp;&nbsp;&nbsp;
+                <i class="fa fa-sign-out"></i> 4 &nbsp;&nbsp;&nbsp;
+                <i class="fa fa-cog"></i> A
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="item">
+        <img src="<?= base_url('assets/assets_shop') ?>/images/product-6-720x480.jpg" alt="">
+        <div class="item-content">
+          <div class="main-content">
+            <div class="meta-category">
+              <span>
+                <i class="fa fa-user"></i> 5 &nbsp;&nbsp;&nbsp;
+                <i class="fa fa-briefcase"></i> 4 &nbsp;&nbsp;&nbsp;
+                <i class="fa fa-sign-out"></i> 4 &nbsp;&nbsp;&nbsp;
+                <i class="fa fa-cog"></i> A
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- Banner Ends Here -->
+
+<section class="blog-posts grid-system">
+  <div class="container">
+    <div class="all-blog-posts">
+      <h2 class="text-center">Offers</h2>
+      <br>
+      <div class="row">
+        <div class="col-md-4 col-sm-6">
+          <div class="blog-post">
+            <div class="blog-thumb">
+              <img src="<?= base_url('assets/assets_mobil') ?>/bmw x5.jpg" alt="">
+            </div>
+            <div class="down-content">
+              <strong>hanya mulai dari</strong> <span>RP. 1.000.000</span> <strong>per hari</strong>
+              <a href="offers.html"><h4>Kalian bisa menggunakan mobil BMW X5</h4></a>
+              <p>BMW F15 X5 (atau F85 untuk X5 M) adalah generasi ketiga dari X5, dan pertama kali diperkenalkan ke publik pada tanggal 30 Mei 2013. Secara resmi memulai debutnya pada bulan November 2013. Ia berbagi sasis dan jarak sumbu roda yang sama dengan E70, dan memperkenalkan tiga pilihan mesin. Kisarannya termasuk xDrive50i (bensin V8 yang menghasilkan 450 bhp), xDrive30d (diesel 6 segaris turbocharge yang menghasilkan 258 bhp), dan M50d Performance (diesel 6 segaris turbocharge yang menghasilkan 381 bhp).</p>
+              <div class="post-options">
+                <div class="row">
+                  <div class="col-lg-12">
+                    <ul class="post-tags">
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4 col-sm-6">
+          <div class="blog-post">
+            <div class="blog-thumb">
+              <img src="<?= base_url('assets/assets_mobil') ?>/lexus ls.jpg" alt="">
+            </div>
+            <div class="down-content">
+              <strong>hanya mulai dari</strong> <span>RP. 1.250.000</span> <strong>per hari</strong>
+              <a href="offers.html"><h4>Kalian bisa menggunakan mobil Lexus LS</h4></a>
+              <p>Lexus LS membuat terobosan dengan klaim mobil pertama di dunia yang menggunakan transmisi 8-speed automatic. Pada mobil lexus LS terdapat fitur Climate Concierge terbagi di dalam 4 area di interior mobil dimana setiap titik ada pengatur udara yang paling nyaman. Terutama untuk tempat duduk belakang. Kita memanfaatkan sinar x yang bisa mengatur suhu tubuh manusia. Jadi teknologi ini bekerja berdasarkan sensor.</p>
+              <div class="post-options">
+                <div class="row">
+                  <div class="col-lg-12">
+                    <ul class="post-tags">
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4 col-sm-6">
+          <div class="blog-post">
+            <div class="blog-thumb">
+              <img src="<?= base_url('assets/assets_mobil') ?>/civic type r.jpg" alt="">
+            </div>
+            <div class="down-content">
+              <strong>hanya mulai dari</strong> <span>RP. 950.000</span> <strong>per hari</strong>
+              <a href="offers.html"><h4>Kalian bisa menggunakan mobil Civic Type R</h4></a>
+              <p>Varian tertinggi hadir dengan mesin Bensin 1996 cc, yang mampu menghasilkan tenaga hingga 306 hp dan torsi puncak 400 Nm. Civic Type R 6-Speed MT berkapasitas 5-penupang dibekali juga dengan transmisi 6-Speed Manual. Sistem keamanannya dibekali Central Locking & Power Door Locks.</p>
+              <div class="post-options">
+                <div class="row">
+                  <div class="col-lg-12">
+                    <ul class="post-tags">
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<section class="call-to-action">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-12">
+        <div class="main-content">
+          <div class="row">
+            <div class="col-lg-8">
+              <span>Ada kendala dalam proses rental mobil?</span>
+              <h4>hubungi kami melalui whatsapp disamping ini</h4>
+            </div>
+            <div class="col-lg-4">
+              <div class="main-button">
+                <a href="https://wa.me/6283108857885">Contact Us</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<div style="height: 180px;"></div>
